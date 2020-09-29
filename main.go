69848db220556438787015a3450b80cc1c45d384
AlfredoCU <alfredo_11_cu@hotmail.com>
2020-09-29 13:14:48 -0500

    Calcular el area de un triangulo.

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
+// Importando nuestras bibliotecas.
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+// Creamos una funcion principal.
+func main() {
+	// Declaramos variables de tipo float64.
+	var (
+		base   uint64
+		height uint64
+	)
+	fmt.Scan(&base) // Tomamos de entrada un valor numérico.
+	fmt.Scan(&height) // Tomamos de entrada un valor numérico.
+
+	// Formula para sacar el área de un triángulo.
+	res := (base * height) / 2
+	fmt.Println(res)
 }
